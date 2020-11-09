73ac7b44585130d12e985927fcb0b3839d28fef5
nkolew <n.kolew@gmail.com>
2020-11-09 15:45:12 +0200

    free space

--- text_rebus.py
+++ text_rebus.py
@@ -3,6 +3,7 @@ def text_rebus(string):
     Input: string // Eines Tages fiel der Esel eines Bauern in den Brunnen
     Output: scrambled words string // Eine Stage Sfie Lde Rese Leine Sbauer Ni Nde Nbrunne
     """
+
     lst = string.split()
     result = ''
 
